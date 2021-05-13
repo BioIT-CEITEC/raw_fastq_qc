27eae5f30c95994126150dbe3e5fc63cc6ff6572
Kateřina Jurásková <245829@muni.cz>
2021-05-13 09:05:11 +0200

    update

--- rules/fastqc.smk
+++ rules/fastqc.smk
@@ -3,7 +3,11 @@ rule merge_fastq_qc:
     input:  html = expand("raw_fastq_qc/{sample}_{read_pair_tag}_fastqc.html",sample = sample_tab.sample_name,read_pair_tag = read_pair_tags)
     output: html = "raw_fastqc.multiqc_report.html",
     log:    "logs/merge_fastq_qc.log"
-    shell:  "touch {output.html}"
+    params:
+        run_log_dir = cfg['bcl2fastq_log_dir'].tolist()[0] if "bcl2fastq_log_dir" in cfg else None,
+    conda: "../wrappers/merge_fastq_qc/env.yaml"
+    script: "../wrappers/merge_fastq_qc/script.py"
+
 
 rule raw_fastq_qc:
     input:  reads = "raw_fastq/{sample}_{read_pair_tag}.fastq.gz"
@@ -11,4 +15,7 @@ rule raw_fastq_qc:
     log:    run = "logs/sample_logs/{sample}/raw_fastq_qc_{read_pair_tag}.log"
     params: extra = "--noextract --format fastq --nogroup"
     threads:  1
-    shell:  "mkdir -p raw_fastq_qc;touch {output}"
+    conda:  "../wrappers/raw_fastq_qc/env.yaml"
+    script: "../wrappers/raw_fastq_qc/script.py"
+
+
---
+++ wrappers/raw_fastq_qc/script.py
@@ -0,0 +1,58 @@
+######################################
+# wrapper for rule: raw_fastq_qc
+######################################
+import os
+import sys
+import math
+import subprocess
+from snakemake.shell import shell
+
+f = open(snakemake.log.run, 'a+')
+f.write("\n##\n## RULE: raw_fastq_qc \n##\n")
+f.close()
+
+TOOL = "fastqc"
+
+shell.executable("/bin/bash")
+
+version = str(subprocess.Popen(TOOL+" --version 2>&1", shell=True, stdout=subprocess.PIPE).communicate()[0], 'utf-8')
+f = open(snakemake.log.run, 'at')
+f.write("## VERSION: "+version+"\n")
+f.close()
+
+command = "mkdir -p `dirname "+snakemake.output.html+"`"+" >> "+snakemake.log.run+" 2>&1"
+f = open(snakemake.log.run, 'at')
+f.write("## COMMAND: "+command+"\n")
+f.close()
+shell(command)
+
+command = TOOL+" -o "+snakemake.params.prefix+" "+snakemake.params.extra+" --threads "+str(snakemake.threads)+" "+snakemake.input.reads+" >> "+snakemake.log.run+" 2>&1 "
+f = open(snakemake.log.run, 'at')
+f.write("## COMMAND: "+command+"\n")
+f.close()
+shell(command)
+
+command = ' sed -r "s:<h2>[ ]*Summary[ ]*<\/h2><ul>:&<li><b>Return to <a href=\'../'+snakemake.params.lib_name+'.final_report.html\'>start page<\/a><\/b><\/li>:" '+snakemake.params.html+' > '+snakemake.output.html
+f = open(snakemake.log.run, 'at')
+f.write("## COMMAND: "+command+"\n")
+f.close()
+shell(command)
+
+command = "rm -f "+snakemake.params.html+" >> "+snakemake.log.run+" 2>&1"
+f = open(snakemake.log.run, 'at')
+f.write("## COMMAND: "+command+"\n")
+f.close()
+shell(command)
+
+
+f = open(snakemake.log.run, 'at')
+command = "cat `ls -tr "+ os.path.dirname(snakemake.log.run) +"/*.log` > "+snakemake.log.run.replace("/raw_fastq_qc_"+snakemake.wildcards.pair+".log",".bcl2fastq.log")
+f.write("## COMMAND: "+command+"\n")
+f.close()
+shell(command)
+
+# OLD STUFF:
+# run:
+#     shell(" {FASTQC} -o {params.prefix} {params.extra} --threads {threads} {input.reads} > {log.run} 2>&1 ")
+#     shell(' sed -r "s:<h2>[ ]*Summary[ ]*<\/h2><ul>:&<li><b>Return to <a href=\'../{wildcards.run_name}.final_report.html\'>start page<\/a><\/b><\/li>:" {params.html} > {output.html} ')
+#     # shell(" mv -T {params.html} {output.html} ")
