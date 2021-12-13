ccf757941ee698f5b90c0361d102495bcf1bc656
Kateřina Jurásková <245829@muni.cz>
2021-12-13 10:29:31 +0100

    first commit

--- wrappers/raw_fastq_qc/script.py
+++ wrappers/raw_fastq_qc/script.py
@@ -59,9 +59,3 @@ f = open(log_filename, 'at')
 f.write("## COMMAND: "+command+"\n")
 f.close()
 shell(command)
-
-# command = ' sed -r -i "s:<h2>[ ]*Summary[ ]*<\/h2><ul>:&<li><b>Return to <a href=\'../'+snakemake.params.lib_name+'.final_report.html\'>start page<\/a><\/b><\/li>:" '+snakemake.output.html
-# f = open(log_filename, 'at')
-# f.write("## COMMAND: "+command+"\n")
-# f.close()
-# shell(command)
