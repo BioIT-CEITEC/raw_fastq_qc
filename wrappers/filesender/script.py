d7628fbd50186fa67a34ac345b17f6257b31a627
Kateřina Jurásková <245829@muni.cz>
2022-03-31 07:54:25 +0200

    first commit

--- wrappers/filesender/script.py
+++ wrappers/filesender/script.py
@@ -16,29 +16,23 @@ f = open(log_filename, 'at')
 f.write("## CONDA: "+version+"\n")
 f.close()
 
-if "@" in snakemake.params.recipient:
+ff = open(str(snakemake.params.credentials))
+filesender_credentials = json.load(ff)
+ff.close()
 
-    ff = open(str(snakemake.params.credentials))
-    filesender_credentials = json.load(ff)
-    ff.close()
+username = filesender_credentials["username"]
+apikey = filesender_credentials["apikey"]
 
-    username = filesender_credentials["username"]
-    apikey = filesender_credentials["apikey"]
+command = "tar -czvf " + snakemake.output.gz + " " + snakemake.input.raw_fastq + " " + snakemake.params.res_file + "* >> " + log_filename + " 2>&1"
+f = open(log_filename, 'at')
+f.write("## COMMAND: " + command + "\n")
+f.close()
+shell(command)
 
-    command = "tar -czvf " + snakemake.output.gz + " " + snakemake.input.raw_fastq + " " + snakemake.params.res_file + "* >> " + log_filename + " 2>&1"
-    f = open(log_filename, 'at')
-    f.write("## COMMAND: " + command + "\n")
-    f.close()
-    shell(command)
+command = "python3 wrappers/filesender/filesender.py -m \"" + snakemake.params.message + "\" -s \"" + snakemake.params.subject + "\" -r " + snakemake.params.recipient + " " + snakemake.output.gz + " >> " + log_filename + " 2>&1" #print command without credentials
+f = open(log_filename, 'at')
+f.write("## COMMAND: " + command + "\n")
+f.close()
+shell("python3 wrappers/filesender/filesender.py -m \"" + snakemake.params.message + "\" -s \"" + snakemake.params.subject + "\" -u " + username + " -a " + apikey + " -r " + snakemake.params.recipient + " " + snakemake.output.gz + " >> " + log_filename + " 2>&1")
 
-    command = "python3 wrappers/filesender/filesender.py -m \"" + snakemake.params.message + "\" -s \"" + snakemake.params.subject + "\" -r " + snakemake.params.recipient + " " + snakemake.output.gz + " >> " + log_filename + " 2>&1" #printing command without credentials
-    f = open(log_filename, 'at')
-    f.write("## COMMAND: " + command + "\n")
-    f.close()
-    shell("python3 wrappers/filesender/filesender.py -m \"" + snakemake.params.message + "\" -s \"" + snakemake.params.subject + "\" -u " + username + " -a " + apikey + " -r " + snakemake.params.recipient + " " + snakemake.output.gz + " >> " + log_filename + " 2>&1")
-
-else:
-    f = open(log_filename, 'at')
-    f.write("## Wrong email address.\n")
-    f.close()
 
